bf5f66724052fbad85907f601b648779ac773d1f
Haris <haris1991@live.com>
2015-01-20 21:29:32 +0100

    GpsTask - Init GPS  manager, get current location

--- app/src/main/java/com/autoskola/instruktori/gps/GpsTask.java
+++ app/src/main/java/com/autoskola/instruktori/gps/GpsTask.java
@@ -1,4 +1,7 @@
 package com.autoskola.instruktori.gps;
+import android.location.LocationManager;
+import android.content.Context;
+import android.location.Location;
 
 /**
  * Created by haris on 1/20/15.
@@ -6,10 +9,52 @@ package com.autoskola.instruktori.gps;
 public class GpsTask {
     private static GpsTask ourInstance = new GpsTask();
 
+    // Location manager
+    private LocationManager mLocationManager;
+
+    /**
+     * NETWORK_PROVIDER - Name of the network location provider.
+     * This provider determines location based on
+     * availability of cell tower and WiFi access points. Results are retrieved
+     * by means of a network lookup.
+     *
+     * GPS_PROVIDER - Name of the GPS location provider.
+     *
+     * This provider determines location using
+     * satellites. Depending on conditions, this provider may take a while to return
+     * a location fix.
+     */
+    private String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
+
+    /**
+     * This is singleton instance for this class
+     */
     public static GpsTask getInstance() {
+
         return ourInstance;
     }
 
+
+    /**
+     * This function is used to init GPS location manager and set GPS provider
+     */
+    public void initGpsManager (Context context, String provider){
+        // Set provider
+        this.LOCATION_PROVIDER = provider;
+
+        // Init location manager
+        if (GpsTask.getInstance().mLocationManager == null) {
+            mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
     private GpsTask() {
+    }
+
+    public void getCurrentGpsLocation (){
+        // Get current location if available
+        Location location = mLocationManager.getLastKnownLocation(LOCATION_PROVIDER);
+        System.out.println("Latitude:"+location.getLatitude());
+        System.out.println("Longitude:"+location.getLongitude());
     }
 }
