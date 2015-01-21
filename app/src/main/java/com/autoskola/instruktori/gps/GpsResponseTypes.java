4ce40a7d8146b09425e6043f81a505315583e9b0
Haris <haris1991@live.com>
2015-01-21 18:32:44 +0100

    Add response types for internet connection

--- app/src/main/java/com/autoskola/instruktori/gps/GpsResponseTypes.java
+++ app/src/main/java/com/autoskola/instruktori/gps/GpsResponseTypes.java
@@ -4,4 +4,12 @@ package com.autoskola.instruktori.gps;
  * Created by haris on 1/20/15.
  */
 public enum GpsResponseTypes {
+
+
+    /**
+     * Internet connection states
+     */
+    WI_FI_CONNECTION,
+    MOBILE_CONNECTION,
+    NO_INTERNET_CONNECTION
 }
