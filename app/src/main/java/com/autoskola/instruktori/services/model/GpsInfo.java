4047b61283d654fc3220a57264a2784d8f33a493
Haris <haris1991@live.com>
2015-01-24 19:33:02 +0100

    Extend GpsInfo model class

--- app/src/main/java/com/autoskola/instruktori/services/model/GpsInfo.java
+++ app/src/main/java/com/autoskola/instruktori/services/model/GpsInfo.java
@@ -1,20 +1,45 @@
 package com.autoskola.instruktori.services.model;
 
+import io.realm.RealmObject;
+
 /**
  * Created by haris on 1/24/15.
  */
-public class GpsInfo {
-    public String voznjaId;
-    public String DetaljiVoznjeId;
-    public double longitude;
-    public double latitude;
+public class GpsInfo extends RealmObject{
+    private String voznjaId;
+    private String detaljiVoznjeId;
+    private double longitude;
+    private double latitude;
 
-    @Override
-    public String toString() {
-        return "GpsInfo{" +
-               // "DetaljiVoznjeId='" + DetaljiVoznjeId + '\'' +
-                ", longitude=" + longitude +
-                ", latitude=" + latitude +
-                '}';
+    public String getVoznjaId() {
+        return voznjaId;
+    }
+
+    public void setVoznjaId(String voznjaId) {
+        this.voznjaId = voznjaId;
+    }
+
+    public String getDetaljiVoznjeId() {
+        return this.detaljiVoznjeId;
+    }
+
+    public void setDetaljiVoznjeId(String detaljiVoznjeId) {
+        this.detaljiVoznjeId = detaljiVoznjeId;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 }
