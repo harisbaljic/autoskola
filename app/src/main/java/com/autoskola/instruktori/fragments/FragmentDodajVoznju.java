1699be50421b1dade038398ae92f5d1a5915cf2d
Haris <haris1991@live.com>
2015-01-25 16:00:47 +0100

    FragmentDodajVoznju - retrofit api call,  Save/Remove item info from app preference,  start/stop  cell color

--- app/src/main/java/com/autoskola/instruktori/fragments/FragmentDodajVoznju.java
+++ app/src/main/java/com/autoskola/instruktori/fragments/FragmentDodajVoznju.java
@@ -9,27 +9,25 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.autoskola.instruktori.R;
 import com.autoskola.instruktori.adapters.VoznjeAdapter;
+import com.autoskola.instruktori.gps.GpsTask;
 import com.autoskola.instruktori.helpers.AppController;
-import com.autoskola.instruktori.helpers.Helper;
-import com.autoskola.instruktori.model.Prijava;
-import com.google.gson.Gson;
+import com.autoskola.instruktori.services.PrijavaWebService;
+import com.autoskola.instruktori.services.model.Prijava;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class FragmentDodajVoznju extends Fragment {
+
     private ListView list;
     private List<Prijava> items = new ArrayList<Prijava>();
     private VoznjeAdapter adapter;
@@ -42,88 +40,165 @@ public class FragmentDodajVoznju extends Fragment {
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onActivityCreated(savedInstanceState);
-        list = (ListView) getActivity().findViewById(
-                R.id.activity_main_list_voznje);
+        list = (ListView) getActivity().findViewById( R.id.activity_main_list_voznje);
 
+        // Instruktor id
+        String korisnikId = AppController.getInstance().getKorisnik().getKorisnikId();
 
-       /* final ProgressDialog progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage("Loading...");
-        progressDialog.setCancelable(false);
-        progressDialog.show(); */
-
-        final String korisnikId = AppController.getInstance().getKorisnik().getKorisnikId();
-
-
-        final StringRequest getAktivnePrijaveRequest = new StringRequest(
-                Request.Method.POST,
-                Helper.aktivnePrijave,
-                new Response.Listener<String>() {
-
-                    @Override
-                    public void onResponse(String arg0) {
-                        Log.d("temp", arg0.toString());
-                        items = Arrays.asList(new Gson().fromJson(
-                                arg0.toString(), Prijava[].class));
-                        Log.d("temp", new Gson().toJson(items));
-                        adapter = new VoznjeAdapter(getActivity(), items);
-                        list.setAdapter(adapter);
-
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError arg0) {
-
-                Log.e("onErrorResponse", arg0.getMessage());
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-
-                params.put("kandidatId", korisnikId + "");
-
-                return params;
-            }
-        };
-
-        AppController.getInstance().addToRequestQueue(getAktivnePrijaveRequest);
+        // Get all aktivne prijave
+        getAktivnePrijave(korisnikId);
 
         // ListView item on click listener
         setListOnClickListener();
     }
 
+    public void getAktivnePrijave(String instruktorId){
+        // Set endpoint
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://projekt001.app.fit.ba/autoskola")
+                .build();
+
+        // Generate service
+        PrijavaWebService service = restAdapter.create(PrijavaWebService.class);
+
+        // Callback
+        Callback<List<com.autoskola.instruktori.services.model.Prijava>> callback = new Callback<List<com.autoskola.instruktori.services.model.Prijava>>() {
+            @Override
+            public void success(List<com.autoskola.instruktori.services.model.Prijava> prijave, Response response) {
+                Log.d("GET Aktivne prijave - success:","");
+                items.addAll(prijave);
+                adapter = new VoznjeAdapter(getActivity(), items);
+                list.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("GET Aktivne prijave - fail:",error.toString());
+            }
+        };
+
+        // GET request
+        service.getAktivnePrijave(instruktorId,callback);
+    }
+
     private void setListOnClickListener (){
         this.list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println("Position:"+position);
+            public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
 
-                new SweetAlertDialog(parent.getContext(), SweetAlertDialog.WARNING_TYPE)
-                        .setTitleText("Da li si siguran?")
-                        .setContentText("Da li zelis da krene pracenje voznje?")
-                        .setConfirmText("Da!")
-                        .showCancelButton(true)
-                        .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                            @Override
-                            public void onClick(SweetAlertDialog sDialog) {
-                                sDialog.cancel();
-                            }
-                        })
-                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                            @Override
-                            public void onClick(SweetAlertDialog sDialog) {
-                                sDialog
-                                        .setTitleText("Odobreno!")
-                                        .setContentText("Kretanje mozete pratiti na mapi.")
-                                        .setConfirmText("Uredu")
-                                        .setConfirmClickListener(null)
-                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
-                            }
-                        })
-                        .show();
+                if (GpsTask.getInstance().getAktivnaPrijava(parent.getContext()) == null){
+                    // Gps task not active
+                    new SweetAlertDialog(parent.getContext(), SweetAlertDialog.WARNING_TYPE)
+                            .setTitleText("Da li si siguran?")
+                            .setContentText("Da li zelis da krene pracenje voznje?")
+                            .setConfirmText("Da!")
+                            .setCancelText("Ne")
+                            .showCancelButton(true)
+                            .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+                                    sDialog.cancel();
+                                }
+                            })
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+                                    sDialog
+                                            .setTitleText("Odobreno!")
+                                            .setContentText("Kretanje mozete pratiti na mapi.")
+                                            .setConfirmText("Uredu")
+                                            .setConfirmClickListener(null)
+                                            .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+
+                                    // Start gps task
+                                    Prijava prijava = items.get(position);
+                                    GpsTask.getInstance().startGPSTask(prijava,parent.getContext());
+                                    adapter.notifyDataSetChanged();
+
+                                }
+                            })
+                            .show();
+
+                }
+                else
+                {
+
+                    // Gps task is active
+                    // Check if selected objects in preference
+                    Prijava selectedPrijava = items.get(position);
+                    if(GpsTask.getInstance().getAktivnaPrijava(parent.getContext()).VoznjaId == selectedPrijava.VoznjaId){
+
+                        new SweetAlertDialog(parent.getContext(), SweetAlertDialog.WARNING_TYPE)
+                                .setTitleText("Da li si siguran?")
+                                .setContentText("Da li zelis prekinuti pracenje?")
+                                .setConfirmText("Da!")
+                                .showCancelButton(true)
+                                .setCancelText("Ne")
+                                .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                    @Override
+                                    public void onClick(SweetAlertDialog sDialog) {
+                                        sDialog.cancel();
+                                    }
+                                })
+                                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                    @Override
+                                    public void onClick(SweetAlertDialog sDialog) {
+                                        sDialog
+                                                .setTitleText("Novo pracenje je zapocelo!")
+                                                .setContentText("Kretanje mozete pratiti na mapi.")
+                                                .setConfirmText("Uredu")
+                                                .setConfirmClickListener(null)
+                                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+
+                                        // Stop gps task
+                                        GpsTask.getInstance().stopGpsTask(parent.getContext());
+                                        adapter.notifyDataSetChanged();
+
+                                    }
+                                })
+                                .show();
+
+                    }
+                    else
+                    {
+                        // Gps task is active
+                        new SweetAlertDialog(parent.getContext(), SweetAlertDialog.WARNING_TYPE)
+                                .setTitleText("Da li si siguran?")
+                                .setContentText("Trenutno postoji aktivna prijava, da li je zelis prekinuti i pokrenuti odabranu?")
+                                .setConfirmText("Da!")
+                                .setCancelText("Ne")
+                                .showCancelButton(true)
+                                .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                    @Override
+                                    public void onClick(SweetAlertDialog sDialog) {
+                                        sDialog.cancel();
+                                    }
+                                })
+                                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                    @Override
+                                    public void onClick(SweetAlertDialog sDialog) {
+                                        sDialog
+                                                .setTitleText("Novo pracenje je zapocelo!")
+                                                .setContentText("Kretanje mozete pratiti na mapi.")
+                                                .setConfirmText("Uredu")
+                                                .setConfirmClickListener(null)
+                                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+
+                                        // Start gps task
+                                        Prijava prijava = items.get(position);
+                                        GpsTask.getInstance().startGPSTask(prijava,parent.getContext());
+                                        adapter.notifyDataSetChanged();
+
+                                    }
+                                })
+                                .show();
+
+                    }
+
+                }
+
             }
         });
     }
