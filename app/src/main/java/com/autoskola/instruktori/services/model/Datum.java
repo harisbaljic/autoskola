1c63231a6006e5b5e0c146f376b1e591606823ec
Haris <haris1991@live.com>
2015-01-25 15:53:24 +0100

    Datum, Prijava - services model class

---
+++ app/src/main/java/com/autoskola/instruktori/services/model/Datum.java
@@ -0,0 +1,14 @@
+package com.autoskola.instruktori.services.model;
+
+/**
+ * Created by haris on 1/25/15.
+ */
+public class Datum {
+
+    //date: "2015-01-20 20:02:03.000000"
+    //timezone_type: 3
+    //timezone: "UTC"
+    public String date;
+    public int timezone_type;
+    public String timezone;
+}
---
+++ app/src/main/java/com/autoskola/instruktori/services/model/Prijava.java
@@ -0,0 +1,32 @@
+package com.autoskola.instruktori.services.model;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by haris on 1/25/15.
+ */
+public class Prijava{
+
+    public int VoznjaId;
+    public Datum DatumPrijave;
+    public int KandidatId;
+    public  Datum DatumVoznje;
+    public  String VrijemeVoznje;
+    public int Aktivno;
+    public String Ime;
+    public String Prezime;
+    public  int InstruktorId;
+
+
+    public String convertToJson() {
+        Gson gson = new Gson();
+        String json = gson.toJson(this);
+        return json;
+    }
+
+    public Prijava convertFromJson(String json) {
+        Gson gson = new Gson();
+        Prijava obj = gson.fromJson(json, Prijava.class);
+        return obj;
+    }
+}
