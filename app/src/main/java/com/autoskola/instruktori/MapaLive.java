a3910b350d6d94fb60e8783d0451b9b84e6ddf1b
Haris <haris1991@live.com>
2015-01-25 17:40:55 +0100

    MapaLive - add gps response handler, draw map route on location change

--- app/src/main/java/com/autoskola/instruktori/MapaLive.java
+++ app/src/main/java/com/autoskola/instruktori/MapaLive.java
@@ -4,17 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.autoskola.instruktori.gps.GpsResponseHandler;
+import com.autoskola.instruktori.gps.GpsResponseTypes;
 import com.autoskola.instruktori.gps.GpsTask;
 import com.autoskola.instruktori.map.MapHelper;
+import com.autoskola.instruktori.services.model.GpsInfo;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.CameraUpdateFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * Created by The Boss on 15.1.2015.
  */
-public class MapaLive extends Activity {
+public class MapaLive extends Activity implements GpsResponseHandler {
 
     private GoogleMap googleMap;
 
@@ -22,15 +30,16 @@ public class MapaLive extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment__mapa);
+        GpsTask.getInstance().communicatorInterfaceMap=this;
 
         try {
             // Loading map
             initilizeMap();
+            updateMap(this);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     /**
@@ -41,6 +50,8 @@ public class MapaLive extends Activity {
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(
                     R.id.map)).getMap();
 
+            //googleMap.setMyLocationEnabled(true);
+
             // check if map is created successfully or not
             if (googleMap == null) {
                 Toast.makeText(getApplicationContext(),
@@ -49,13 +60,46 @@ public class MapaLive extends Activity {
             }
         }
 
-        // Show current location on map
-        MapHelper.getInstance().setMapToLocation(GpsTask.getInstance().getCurrentGpsLocation(),googleMap);
+     MapHelper.getInstance().setMapToLocation(GpsTask.getInstance().getCurrentGpsLocation(),googleMap);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        initilizeMap();
+    }
+
+    @Override
+    public void onGpsResponse(GpsResponseTypes responseType) {
+      if(responseType == GpsResponseTypes.GPS_LOCATION_CHANGED){
+          // Update map
+         updateMap(this);
+      }
+   }
+
+  private void updateMap (final Activity activity){
+      // Check for active prijava
+      if (GpsTask.getInstance().getAktivnaPrijava(getBaseContext())!=null) {
+          new Thread(new Runnable() {
+              public void run() {
+
+                  Realm realm = Realm.getInstance(getBaseContext());
+                  RealmResults<GpsInfo> gpsList = realm.where(GpsInfo.class)
+                          .equalTo("voznjaId", GpsTask.getInstance().getAktivnaPrijava(getBaseContext()).VoznjaId)
+                          .findAll();
+
+
+                  List<LatLng> locations = new ArrayList<LatLng>();
+                  for(int i=0;i<gpsList.size();i++){
+                      locations.add(new LatLng(Double.valueOf(gpsList.get(i).getLatitude()),Double.valueOf(gpsList.get(i).getLongitude())));
+
+                  }
+
+                  MapHelper.getInstance().drawMapRoute(locations,googleMap,activity);
+                  //new LatLng(43.856259,18.413076);
+
+              }
+          }).start();
+      }
+
     }
 }
