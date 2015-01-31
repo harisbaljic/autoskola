5e9f9f9dd54701eb4c310fbd0be6df1553327b47
Haris <haris1991@live.com>
2015-01-31 16:38:07 +0100

    Change FragmentPrijave to v4 support Fragment

--- app/src/main/java/com/autoskola/instruktori/fragments/FragmentPrijave.java
+++ app/src/main/java/com/autoskola/instruktori/fragments/FragmentPrijave.java
@@ -1,19 +1,13 @@
 package com.autoskola.instruktori.fragments;
 
-import android.app.Activity;
 import android.app.Dialog;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -33,7 +27,7 @@ import java.util.List;
 import java.util.Map;
 
 
-public class FragmentPrijave extends Fragment {
+public class FragmentPrijave extends android.support.v4.app.Fragment {
 
     private ListView list;
     private List<Prijava> items = new ArrayList<Prijava>();
