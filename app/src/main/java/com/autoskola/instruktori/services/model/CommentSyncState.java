e404ff16b8969318e22d541b6c97a29efad2839b
Haris <haris1991@live.com>
2015-02-01 17:35:22 +0100

    Sync states

---
+++ app/src/main/java/com/autoskola/instruktori/services/model/CommentSyncState.java
@@ -0,0 +1,10 @@
+package com.autoskola.instruktori.services.model;
+
+/**
+ * Created by haris on 2/1/15.
+ */
+public enum CommentSyncState {
+    COMMENT_SYNC_NO,
+    COMMENT_SYNC_IN_PROGRESS,
+    COMMENT_SYNC_YES
+}
