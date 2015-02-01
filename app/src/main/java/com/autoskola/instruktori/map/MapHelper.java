70ca00d8c491e5cd538d9cbb48bccb5343e539af
Haris <haris1991@live.com>
2015-02-01 17:36:37 +0100

    MapHelper improvements

--- app/src/main/java/com/autoskola/instruktori/map/MapHelper.java
+++ app/src/main/java/com/autoskola/instruktori/map/MapHelper.java
@@ -46,6 +46,18 @@ public class MapHelper {
         map.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 
+    public void setMapToLocation(double lat , double lng,GoogleMap map){
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(lat, lng);
+
+        // Showing the current location in Google Map
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+
     public void drawMapRoute (final List<LatLng> routes,final GoogleMap googleMap, Activity context){
 
         if (googleMap != null) {
@@ -53,10 +65,13 @@ public class MapHelper {
                     @Override
 
                     public void run() {
-                        if (routes.size()>0)
-                        // Add map makers
-                        googleMap.addMarker(new MarkerOptions().position(routes.get(0))
-                                .title("First Point"));
+                        if (routes.size()>0) {
+                            // Add map makers{
+                            googleMap.addMarker(new MarkerOptions().position(routes.get(0))
+                                    .title("First Point"));
+
+                            setMapToLocation(routes.get(0).latitude,routes.get(0).longitude,googleMap);
+                        }
 
                         // Add lines on map
                         googleMap.addPolyline(new PolylineOptions()
