8479ac11af2ade626274d351eff5d9bbab7b6d89
Haris <haris1991@live.com>
2015-02-01 17:37:57 +0100

    ZavrseneVoznjeDetalji

--- app/src/main/java/com/autoskola/instruktori/fragments/FragmentZavrseneVoznjeDetalji.java
+++ app/src/main/java/com/autoskola/instruktori/fragments/FragmentZavrseneVoznjeDetalji.java
@@ -8,13 +8,17 @@ import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.autoskola.instruktori.R;
+import com.autoskola.instruktori.adapters.CommentAdapter;
 import com.autoskola.instruktori.map.MapHelper;
 import com.autoskola.instruktori.services.GpsWebService;
 import com.autoskola.instruktori.services.model.GpsInfo;
+import com.autoskola.instruktori.services.model.Komentar;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -35,6 +39,9 @@ public class FragmentZavrseneVoznjeDetalji extends DialogFragment {
     private ListView mListViewKomentari;
     private GoogleMap googleMap;
     private static View view;
+    private TextView mTxtEmptyCommentsView;
+    private CommentAdapter mCommentAdapter;
+
 
 
     public static final FragmentZavrseneVoznjeDetalji newInstance(String voznjaId)
@@ -60,16 +67,33 @@ public class FragmentZavrseneVoznjeDetalji extends DialogFragment {
         } catch (InflateException e) {
         /* map is already there, just return view as it is */
         }
+
+        Button btnClose = (Button)view.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCommentAdapter = new CommentAdapter(getActivity(),new ArrayList<Komentar>());
+                mListViewKomentari.setAdapter(mCommentAdapter);
+                getDialog().dismiss();
+            }
+        });
+
+        mTxtEmptyCommentsView = (TextView)view.findViewById(R.id.emptyView);
+        mListViewKomentari = (ListView)view.findViewById(R.id.list_view_komentari);
         return view;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         String voznjaId  = getArguments().getString("voznjaId");
 
         // Get gps map info
         getMapDetails(voznjaId);
+
+        // Get comments
+        getComments(voznjaId);
     }
 
     @Override
@@ -136,6 +160,41 @@ public class FragmentZavrseneVoznjeDetalji extends DialogFragment {
         // GET request
         service.getGpsInfo(voznjaId,callback);
 
+    }
+
+    private void getComments (String voznjaId){
+
+        // Set endpoint
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://projekt001.app.fit.ba/autoskola")
+
+                .build();
+
+        // Generate service
+        GpsWebService service = restAdapter.create(GpsWebService.class);
+
+        // Callback
+        Callback<List<Komentar>> callback = new Callback<List<Komentar>>() {
+            @Override
+            public void success(List<Komentar> commentList, Response response) {
+                Log.d("GET comments - success:", "");
+                 if (commentList.size()==0)
+                     mTxtEmptyCommentsView.setVisibility(View.VISIBLE);
+                else
+                     mTxtEmptyCommentsView.setVisibility(View.INVISIBLE);
+
+                mCommentAdapter = new CommentAdapter(getActivity(),commentList);
+                mListViewKomentari.setAdapter(mCommentAdapter);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("GET comments - fail:",error.toString());
+            }
+        };
+
+        // GET request
+        service.getGpsKomentar(voznjaId, callback);
     }
 
 
