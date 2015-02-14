c5cbc8c00667da0064ed66e37448cb1769c53bbf
Haris <haris1991@live.com>
2015-02-14 16:50:50 +0100

    CommentWebService test

---
+++ app/src/main/java/com/autoskola/instruktori/test/CommentWebServiceTest.java
@@ -0,0 +1,15 @@
+package com.autoskola.instruktori.test;
+
+/**
+ * Created by haris on 2/14/15.
+ */
+public class CommentWebServiceTest {
+    private static CommentWebServiceTest ourInstance = new CommentWebServiceTest();
+
+    public static CommentWebServiceTest getInstance() {
+        return ourInstance;
+    }
+
+    private CommentWebServiceTest() {
+    }
+}
