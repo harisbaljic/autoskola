3d15602d818884de4483a31d5538c44a478b2462
Haris <haris1991@live.com>
2015-02-14 17:05:24 +0100

    Refactoring

--- app/src/main/java/com/autoskola/instruktori/services/model/DataSyncState.java
+++ app/src/main/java/com/autoskola/instruktori/services/model/DataSyncState.java
@@ -3,7 +3,7 @@ package com.autoskola.instruktori.services.model;
 /**
  * Created by haris on 2/1/15.
  */
-public enum CommentSyncState {
+public enum DataSyncState {
     COMMENT_SYNC_NO,
     COMMENT_SYNC_IN_PROGRESS,
     COMMENT_SYNC_YES
